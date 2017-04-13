929ac08a5a08280fc8185ed8ea71d430dc376885
curtisgreene <curtis.j.w.greene@gmail.com>
2017-04-13 15:01:48 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if params[:name].present?
+      session[:name] = params[:name]
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    session.clear
+    redirect_to login_path
+  end
+end
